2778bd67b16964265e9113469cacf30a2f0bf2b0
Luca Gorosito <lucagoro04@gmail.com>
2026-02-11 20:26:19 -0300

    seteo para deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080', // Replace with your backend URL
+    // Usa la variable de entorno, y si no existe (local), usa localhost
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
 });
 
 // Interceptor para meter el token en cada petición
